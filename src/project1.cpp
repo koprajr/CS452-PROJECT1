d675bf8be8793be52dc69b545c6f962f38c7b280
Jeff <koprajr@gmail.com>
2014-04-23 11:48:03 -0400

    take 4

--- src/project1.cpp
+++ src/project1.cpp
@@ -379,7 +379,13 @@ void display() {
 		glLoadIdentity();
 		glColor3f(0.0, 1.0, 0.0);
 		glRasterPos2i(10, 10);
-		string s = "Game Over, your score is:" + lexical_cast<string>(score);
+
+		int a = 10;
+		stringstream ss;
+		ss << score;
+		string s = ss.str();
+
+		//string s = "Game Over, your score is:" + lexical_cast<string>(score);
 		void * font = GLUT_BITMAP_9_BY_15;
 		for (string::iterator i = s.begin(); i != s.end(); ++i)
 		{
